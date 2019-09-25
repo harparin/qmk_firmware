a91a090da952d117cc458cafb3bbc7e3b93d28bb
Ansgar Himmel <ansgar.himmel@rewe-digital.com>
2019-09-25 17:12:04 +0200

    refactored symbol layer to use speaking names (DE_***)

--- keyboards/atreus/keymaps/ansgar/config.h
+++ keyboards/atreus/keymaps/ansgar/config.h
@@ -16,5 +16,3 @@
 
 #define TAPPING_TERM 260
 
-#define MATRIX_ROW_PINS { B4, B2, B5, B6 }
-#define MATRIX_COL_PINS { F6, F5, F7, B1, B3, E6, D7, C6, D4, D0, D1 }
--- keyboards/atreus/keymaps/ansgar/keymap.c
+++ keyboards/atreus/keymaps/ansgar/keymap.c
@@ -9,34 +9,55 @@
 #define BROWSER_CONTROL 4
 
 
-#define KC_UE KC_LBRC
-#define KC_OE KC_SCLN
-#define KC_AE KC_QUOT
-#define KC_SZ KC_MINUS
 #define MOD_CMD MOD_LGUI 
+
+#define DE_UE    KC_LBRC
+#define DE_OE    KC_SCLN
+#define DE_AE    KC_QUOT
+#define DE_SZ    KC_MINUS
+#define DE_HASH  KC_BSLS
+
+#define DE_AT    A(KC_L)
+#define DE_AMPRS S(KC_6)
+#define DE_DLLR  S(KC_4)
+#define DE_PRCT  S(KC_5)
+#define DE_LCBR  A(KC_8)
+#define DE_RCBR  A(KC_9)
+#define DE_FSLS  KC_AMPR
+#define DE_BSLS  A(S(KC_7))
+#define DE_LPRN  S(KC_8)
+#define DE_RPRN  S(KC_9)
+#define DE_LBKT  A(KC_5)
+#define DE_RBKT  A(KC_6)
+#define DE_LT    KC_NUBS
+#define DE_GT    S(KC_NUBS)
+#define DE_MINS  KC_SLSH
+#define DE_PIPE  A(KC_7)
+#define DE_PARA  S(KC_3)
+#define DE_ASTR  S(KC_RBRC)
+#define DE_EQUL  S(KC_0)
+#define DE_PLUS  KC_RBRC
 
 
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
-  KC_APOs,
-  KC_SPKL,
-  DE_BSPC
+
 };
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LYR_BASE] = LAYOUT(
-    	KC_UE,                KC_O,        KC_E,          KC_P,  KC_Z,                                                             KC_F,                KC_G,  KC_T,          KC_N,          KC_L, 
-	KC_A,                 KC_COMM,     KC_DOT,        KC_U,  KC_I,                                                             KC_D,                KC_H,  KC_C,          KC_R,          KC_S, 
-	MT(MOD_LSFT, KC_OE),  KC_Q,        KC_J,          KC_K,  KC_X,                                                             KC_B,                KC_M,  KC_W,          KC_V,          MT(MOD_RSFT, KC_Y), 
- 	MT(MOD_LCTL, KC_TAB), LT(3,KC_AE), LT(2,KC_BSLS), MO(1), MT(MOD_CMD, KC_BSPC), MT(MOD_RALT, KC_DEL), MT(MOD_RALT, KC_ENT), MT(MOD_CMD, KC_SPC), MO(1), LT(2,KC_2),     LT(3,KC_MINS), MT(MOD_LCTL, KC_ESC)
+    DE_UE,                KC_O,        KC_E,          KC_P,  KC_Z,                                                             KC_F,                KC_G,  KC_T,          KC_N,          KC_L, 
+	  KC_A,                 KC_COMM,     KC_DOT,        KC_U,  KC_I,                                                             KC_D,                KC_H,  KC_C,          KC_R,          KC_S, 
+	  MT(MOD_LSFT, DE_OE),  KC_Q,        KC_J,          KC_K,  KC_X,                                                             KC_B,                KC_M,  KC_W,          KC_V,          MT(MOD_RSFT, KC_Y), 
+ 	  MT(MOD_LCTL, KC_TAB), LT(3,DE_AE), LT(2,DE_HASH), MO(1), MT(MOD_CMD, KC_BSPC), MT(MOD_RALT, KC_DEL), MT(MOD_RALT, KC_ENT), MT(MOD_CMD, KC_SPC), MO(1), LT(2,KC_2),     LT(3,DE_SZ), MT(MOD_LCTL, KC_ESC)
   ),
 
   [LYR_NUMS] = LAYOUT(
-    KC_EXLM, KC_HASH,  A(S(KC_7)),    A(KC_8), A(KC_9),                   KC_ASTR,  KC_7, KC_8,   KC_9,    S(KC_3),
-    A(KC_L), S(KC_6),  KC_AMPR,    S(KC_8), S(KC_9),                   KC_EQUAL, KC_4, KC_5,   KC_6,    KC_SLSH,
-    A(KC_7), KC_NUBS,  S(KC_NUBS), A(KC_5), A(KC_6),                   KC_PLUS,  KC_1, KC_2,   KC_3,    KC_TRNS,
-    KC_TRNS, KC_TRNS,  KC_BSLS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_0, KC_DOT, KC_COMM, KC_TRNS
+    DE_DLLR, DE_PRCT,  DE_BSLS,    DE_LCBR, DE_RCBR,                   DE_ASTR,  KC_7, KC_8,   KC_9,    DE_PARA,
+    DE_AT,   DE_AMPRS, DE_FSLS,    DE_LPRN, DE_RPRN,                   DE_EQUL,  KC_4, KC_5,   KC_6,    DE_MINS,
+    DE_PIPE, DE_LT,    DE_GT,      DE_LBKT, DE_RBKT,                   DE_PLUS,  KC_1, KC_2,   KC_3,    KC_TRNS,
+    KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_0, KC_DOT, KC_COMM, KC_TRNS
   ),
 
   [LYR_NAV] = LAYOUT(
@@ -49,30 +70,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [LYR_FUNC] = LAYOUT(
     RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ), 
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {  
-  switch (keycode) {
-    case DE_BSPC:
-      if (record->event.pressed) {
-        SEND_STRING("f");
-      }
-      break;
-
-    case KC_SPKL:
-      if (record->event.pressed) {
-        SEND_STRING("~");
-      }
-      break;
-    }
-  return true;
-}
-
-void matrix_init_user(void) {
-    set_unicode_input_mode(UC_OSX);
-}
 
 
